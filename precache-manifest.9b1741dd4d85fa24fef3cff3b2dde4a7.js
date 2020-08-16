4d81df5baa9959eb968ceba282d6d225c8979b98
wexiicek <ujexxi@gmail.com>
2020-08-16 16:00:00 +0200

    Updates

---
+++ precache-manifest.9b1741dd4d85fa24fef3cff3b2dde4a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db21b0bec8c050d55c8c3cab35bc1f6d",
+    "url": "/aeropress-timer/index.html"
+  },
+  {
+    "revision": "480e9150b37a22ba4c6c",
+    "url": "/aeropress-timer/static/css/main.53f744ad.chunk.css"
+  },
+  {
+    "revision": "850416a3ff88af125eb0",
+    "url": "/aeropress-timer/static/js/2.575408f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aeropress-timer/static/js/2.575408f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940d508c99b41280fbd7",
+    "url": "/aeropress-timer/static/js/3.cf5b5777.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/aeropress-timer/static/js/3.cf5b5777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480e9150b37a22ba4c6c",
+    "url": "/aeropress-timer/static/js/main.4bf188a3.chunk.js"
+  },
+  {
+    "revision": "c34f97c695fb9953ec33",
+    "url": "/aeropress-timer/static/js/runtime-main.5a256c91.js"
+  },
+  {
+    "revision": "53f632f2cd173725dd9228164d333a75",
+    "url": "/aeropress-timer/static/media/bell.53f632f2.mp3"
+  }
+]);
